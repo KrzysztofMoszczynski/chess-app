ab2b991965e9f91394ed95e3e640b273b1ad9b26
KrzysztofMoszczynski <luxtorpeda0104@gmail.com>
2021-05-19 22:30:55 +0200

    Changed filesystem

--- app/src/main/java/com/chess_app/view/MainActivity.java
+++ app/src/main/java/com/chess_app/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.chess_app;
+package com.chess_app.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.chess_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
